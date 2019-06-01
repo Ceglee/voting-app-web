36f05e244d3f9a9741ed5bf52fa576a9ac3efbac
CegLee <ceglarzp@gmail.com>
2019-06-01 19:12:31 +0100

    loader for login component

--- src/app/components/login-component/login-component.component.ts
+++ src/app/components/login-component/login-component.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponentComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean;
+
+  constructor() {
+    this.loading = false;
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    this.loading = true;
+  }
 }
