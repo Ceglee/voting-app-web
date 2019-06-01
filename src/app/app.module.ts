18888a8981795dca3d57794f65e77a7d5dd43ad2
CegLee <ceglarzp@gmail.com>
2019-06-01 16:50:49 +0100

    login page implementation, component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '@components/app-root/app.component';
+import { LoginComponentComponent } from '@components/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule
